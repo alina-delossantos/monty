e66af61b4b7f1d07d2ab76e0393946a97376fc38
root <alina.delossantos@gmail.com>
2022-01-16 00:19:18 +0000

    added sub

--- 2_opcode_implementation.c
+++ 2_opcode_implementation.c
@@ -39,3 +39,26 @@ void add_fx(stack_t **h, unsigned int ln_nbr)
 	*h = holder->next;
 	free(holder);
 }
+
+/**
+ * sub_fx - subtracts the top element of the stack from the second top element
+ * @stack: stack
+ * @ln_nbr: line number
+ */
+
+void sub_fx(stack_t **h, unsigned int ln_nbr)
+{
+	stack_t *holder = NULL;
+	int diff = 0;
+
+	holder = *stack;
+	if (holder == NULL || holder->next == NULL)
+	{
+		printf("L%d: can't sub, stack too short\n", ln_nbr);
+		exit(EXIT_FAILURE);
+	}
+	diff = holder->next->n - holder->n;
+	holder->next->n = diff;
+
+	pop_func(h, ln_nbr);
+}
